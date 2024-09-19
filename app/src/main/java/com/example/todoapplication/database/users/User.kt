7adf9465572751f3c9424ec36e4e06dad88ed945
DHuyenTrang <huyentrang111120044@gmail.com>
2024-09-19 11:13:17 +0700

    update: import lib

---
+++ app/src/main/java/com/example/todoapplication/database/users/User.kt
@@ -0,0 +1,15 @@
+package com.example.todoapplication.database.users
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(
+    @ColumnInfo var username: String,
+    @ColumnInfo var password: String,
+    @ColumnInfo var email: String
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+}
